8b66ffa6d6b9bd26ab017e6c20aa526ccafcaaac
jaspal <jaspalchandramola29@gmail.com>
2018-07-27 08:28:07 +0530

    upated Text Verify commit

--- TextVerify.py
+++ TextVerify.py
@@ -1,31 +1,19 @@
 import nltk
-# f=open("English","r")
-# # For English 
-# Englishtext=f.read()
-# sent_text = nltk.sent_tokenize(Englishtext) # this gives us a list of sentences
-# print("English")
-# print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# i=0
-# for sent in sent_text:
-#     print(str(i)+" "+sent)
-#     i=i+1
-# f.close()
-# # For French 
-# f=open("French","r")
-# Frenchtext=f.read()
-# sent_text = nltk.sent_tokenize(Frenchtext,language='french') # this gives us a list of sentences
-# print("French")
-# print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# i=0
-# for sent in sent_text:
-#     print(str(i)+" "+sent)
-#     i=i+1
-# f.close()
-
-# For Hindi 
-f=open("Hindi","r")
+f=open("English","r")
+# For English 
+Englishtext=f.read()
+sent_text = nltk.sent_tokenize(Englishtext) # this gives us a list of sentences
+print("English")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+i=0
+for sent in sent_text:
+    print(str(i)+" "+sent)
+    i=i+1
+f.close()
+# For French 
+f=open("French","r")
 Frenchtext=f.read()
-sent_text = nltk.sent_tokenize(Frenchtext,language='indian') # this gives us a list of sentences
+sent_text = nltk.sent_tokenize(Frenchtext,language='french') # this gives us a list of sentences
 print("French")
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
 i=0
@@ -33,5 +21,6 @@ for sent in sent_text:
     print(str(i)+" "+sent)
     i=i+1
 f.close()
+
 
 
